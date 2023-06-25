b583fec67faf7b77b16aa6a8ca3250090d1862dc
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-06-25 21:51:33 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int result = 0;
+
+        for(int i =1; i < nums.length; i++){
+            if(nums[i] <= nums[i-1]){
+                result+= Math.abs(nums[i-1]-nums[i])+1;
+                nums[i] = nums[i-1]+1;
+            }
+        }
+        return result;
+    }
+}
